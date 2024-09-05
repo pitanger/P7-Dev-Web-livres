43d69df1c607916ffa06fc43f14d88c019ce57e5
pitanger <mathieu.krause@hotmail.com>
2024-09-05 18:01:11 +0200

    modif package et serv adress

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const app = require("./app");
+const app = require("./routes/app");
 const normalizePort = (val) => {
     const port = parseInt(val, 10);
     if (isNaN(port)) {
@@ -10,7 +10,7 @@ const normalizePort = (val) => {
     }
     return false;
 };
-const port = normalizePort(process.env.PORT || "3000");
+const port = normalizePort(process.env.PORT || "4000");
 app.set("port", port);
 const errorHandler = (error) => {
     if (error.syscall !== "listen") {
