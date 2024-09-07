580ed595904a3a63bdd2a6c9bbd799e08ba493a2
pitanger <mathieu.krause@hotmail.com>
2024-09-07 20:03:18 +0200

    git ignore node modules

--- Backend/models/book.js
+++ Backend/models/book.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const bookSchema = mongoose.Schema({
+  userId: { type: String, required: true },
   title: { type: String, required: true },
   author: { type: String, required: true },
   year: { type: Number, required: true },
